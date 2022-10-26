174334450ae789de104bdad959de5168157ea051
serinHeo <serin9811@gmail.com>
2022-10-26 23:12:59 +0900

    init

---
+++ src/main/java/kr/dearme/dearmeapi/DearmeApiApplication.java
@@ -0,0 +1,13 @@
+package kr.dearme.dearmeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearmeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearmeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/dearme/dearmeapi/DearmeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.dearme.dearmeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearmeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
