cfde8ecd83f93e2d801c902718cd9018608a62c2
serinHeo <serin9811@gmail.com>
2022-10-29 16:52:20 +0900

    initial commit

---
+++ src/main/java/kr/snailemail/snailemail/SnailEmailApplication.java
@@ -0,0 +1,13 @@
+package kr.snailemail.snailemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnailEmailApplication.class, args);
+	}
+
+}
