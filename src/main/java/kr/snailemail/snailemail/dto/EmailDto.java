d8f591a8adfd5afe5b7a758ad84907dcfc3b0029
serinheo <serin9811@gmail.com>
2022-11-22 23:32:51 +0900

    add: add email dto

---
+++ src/main/java/kr/snailemail/snailemail/dto/EmailDto.java
@@ -0,0 +1,14 @@
+package kr.snailemail.snailemail.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailDto {
+
+    private String userId;
+    private String emailTitle;
+    private String emailContent;
+
+}
